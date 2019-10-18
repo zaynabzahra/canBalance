4742b7927d3d28f3450b683b95c129a491ef6fc1
Zaynab Zahra <zaynabzahra@gmail.com>
2019-10-18 20:36:26 +0100

    -developing method

--- canBalance.java
+++ canBalance.java
@@ -1,23 +1,28 @@
+import java.util.Arrays;
 public class canBalance {
 
 
 public boolean testBalance (int values[])
 {
+int[] val1;
+int[] val2;
 
-    int idea;
+int length = values.length;
+int temp = values[0];
 
+val1[0] = temp;
+
+for (int i= 1; i <= (length-1); i ++){
+
+    temp = temp + values[i];
+
+    val1[i] = temp;
+
+
+}
+ System.out.println(Arrays.toString(val1));
 
     return true;
 }
-
-
-
-
-
-
-
-
-
-
 
 }
